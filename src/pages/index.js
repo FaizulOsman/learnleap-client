b54aee1836f288a6becbeac9f1ed457daeb52c55
FaizulOsman <faizulosmanrabby@gmail.com>
2023-08-19 19:53:19 +0600

    feat: show tests in home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import FindExamBySubject from "@/components/UI/FindExamBySubject";
+import FindTestBySubject from "@/components/UI/FindTestBySubject";
 import RootLayout from "@/components/layouts/RootLayout";
 import { useGetAllExamQuery } from "@/redux/exam/examApi";
 import { useState } from "react";
 
 const HomePage = () => {
   const { data: allExam } = useGetAllExamQuery();
-  const [category, setCategory] = useState("English");
+  const [examCategory, setExamCategory] = useState("English");
+  const [testCategory, setTestCategory] = useState("English");
 
   const uniqueSubjects = [];
   allExam?.data?.map((exam) => {
@@ -22,30 +24,55 @@ const HomePage = () => {
     }
   });
 
-  const filterBySubject = allExam?.data?.filter((exam) => {
-    return exam?.subject === category;
+  const filterByExamSubject = allExam?.data?.filter((exam) => {
+    return exam?.subject === examCategory;
+  });
+
+  const filterByTestSubject = allExam?.data?.filter((exam) => {
+    return exam?.subject === testCategory;
   });
 
   return (
-    <div>
-      <h1 className="text-3xl font-semibold text-center my-8">Exams</h1>
-      <div className="w-10/12 md:w-8/12 mx-auto flex justify-between">
-        {uniqueSubjects?.map((exam, index) => (
-          <div
-            key={index}
-            onClick={() => setCategory(exam?.subject)}
-            className="bg-gray-200 px-2 py-1 rounded-md cursor-pointer hover:bg-gray-300 hover:text-white font-semibold"
-          >
-            {exam?.subject}
-          </div>
-        ))}
+    <>
+      <div>
+        <h1 className="text-3xl font-semibold text-center my-8">Exams</h1>
+        <div className="w-10/12 md:w-8/12 mx-auto flex justify-between">
+          {uniqueSubjects?.map((exam, index) => (
+            <div
+              key={index}
+              onClick={() => setExamCategory(exam?.subject)}
+              className="bg-gray-200 px-2 py-1 rounded-md cursor-pointer hover:bg-gray-300 hover:text-white font-semibold"
+            >
+              {exam?.subject}
+            </div>
+          ))}
+        </div>
+        <div className="mt-10 flex flex-col gap-5">
+          {filterByExamSubject?.map((exam, index) => (
+            <FindExamBySubject key={index} exam={exam} />
+          ))}
+        </div>
       </div>
-      <div className="mt-10 flex flex-col gap-5">
-        {filterBySubject?.map((exam, index) => (
-          <FindExamBySubject key={index} exam={exam} />
-        ))}
+      <div className="mt-20">
+        <h1 className="text-3xl font-semibold text-center my-8">Tests</h1>
+        <div className="w-10/12 md:w-8/12 mx-auto flex justify-between">
+          {uniqueSubjects?.map((test, index) => (
+            <div
+              key={index}
+              onClick={() => setTestCategory(test?.subject)}
+              className="bg-gray-200 px-2 py-1 rounded-md cursor-pointer hover:bg-gray-300 hover:text-white font-semibold"
+            >
+              {test?.subject}
+            </div>
+          ))}
+        </div>
+        <div className="mt-10 flex flex-col gap-5">
+          {filterByTestSubject?.map((test, index) => (
+            <FindTestBySubject key={index} test={test} />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
