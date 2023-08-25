46b4eaaf1fffd3ce31569dcdf0def37289794019
FaizulOsman <faizulosmanrabby@gmail.com>
2023-08-25 20:42:27 +0600

    [feat] loader added in single exam page

--- src/pages/exam/[...segments].jsx
+++ src/pages/exam/[...segments].jsx
@@ -1,4 +1,5 @@
 import ExamSingleQues from "@/components/UI/ExamSingleQues";
+import Loader from "@/components/UI/Loader";
 import Stopwatch from "@/components/UI/Stopwatch";
 import RootLayout from "@/components/layouts/RootLayout";
 import {
@@ -123,55 +124,68 @@ const SingleExam = () => {
           <h2 className="text-3xl font-semibold text-center">
             {segments?.[0]} Exam {getSingleExam?.data?.serial}
           </h2>
-          <div className="my-5">
-            <Stopwatch
-              isRunning={isRunning}
-              setIsRunning={setIsRunning}
-              timeLimit={getSingleExam?.data?.timeLimit}
-              setTimeOver={setTimeOver}
-            />
-          </div>
-          <div className="grid grid-cols-1 gap-4">
-            {getSingleExam?.data?.questions?.map((exam, index) => (
-              <ExamSingleQues
-                key={index}
-                exam={exam}
-                index={index}
-                count={count}
-                setCount={setCount}
-                ques={ques}
-                setQues={setQues}
-                eyeShow={eyeShow}
-              />
-            ))}
-          </div>
-          <div className="text-center mt-4">
-            <label
-              htmlFor="my-modal-4"
-              className="btn bg-green-500 text-white hover:bg-green-600 modal-button"
-              onClick={() => handleSubmitExam()}
-              ref={submitButtonRef}
-            >
-              Submit Exam
-            </label>
+          {getSingleExam?.data?.questions &&
+          getSingleExam?.data?.questions.length > 0 ? (
             <>
-              <input type="checkbox" id="my-modal-4" className="modal-toggle" />
-              <label htmlFor="my-modal-4" className="modal cursor-pointer">
-                <label className="modal-box relative py-10" htmlFor="">
-                  <h3 className="text-lg font-bold text-green-500">
-                    Total Correct Answer: {count}
-                  </h3>
-                  <h3 className="text-lg font-bold text-red-500">
-                    Total Wrong Answer: {wrong}
-                  </h3>
-                  <h3 className="text-xl font-bold text-green-500 pt-2">
-                    You Got: {totalMark}
-                  </h3>
-                  <p className="pt-3">Keep going...!!!</p>
+              <div className="my-5">
+                <Stopwatch
+                  isRunning={isRunning}
+                  setIsRunning={setIsRunning}
+                  timeLimit={getSingleExam?.data?.timeLimit}
+                  setTimeOver={setTimeOver}
+                />
+              </div>
+              <div className="grid grid-cols-1 gap-4">
+                {getSingleExam?.data?.questions?.map((exam, index) => (
+                  <ExamSingleQues
+                    key={index}
+                    exam={exam}
+                    index={index}
+                    count={count}
+                    setCount={setCount}
+                    ques={ques}
+                    setQues={setQues}
+                    eyeShow={eyeShow}
+                  />
+                ))}
+              </div>
+              <div className="text-center mt-4">
+                <label
+                  htmlFor="my-modal-4"
+                  className="btn bg-green-500 text-white hover:bg-green-600 modal-button"
+                  onClick={() => handleSubmitExam()}
+                  ref={submitButtonRef}
+                >
+                  Submit Exam
                 </label>
-              </label>
+                <>
+                  <input
+                    type="checkbox"
+                    id="my-modal-4"
+                    className="modal-toggle"
+                  />
+                  <label htmlFor="my-modal-4" className="modal cursor-pointer">
+                    <label className="modal-box relative py-10" htmlFor="">
+                      <h3 className="text-lg font-bold text-green-500">
+                        Total Correct Answer: {count}
+                      </h3>
+                      <h3 className="text-lg font-bold text-red-500">
+                        Total Wrong Answer: {wrong}
+                      </h3>
+                      <h3 className="text-xl font-bold text-green-500 pt-2">
+                        You Got: {totalMark}
+                      </h3>
+                      <p className="pt-3">Keep going...!!!</p>
+                    </label>
+                  </label>
+                </>
+              </div>
             </>
-          </div>
+          ) : (
+            <div className="min-h-[50vh] flex items-center">
+              <Loader />
+            </div>
+          )}
         </div>
       </>
     </div>
