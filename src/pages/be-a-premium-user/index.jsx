55586283519bb9af139c509853e87b4b750e3be7
FaizulOsman <faizulosmanrabby@gmail.com>
2023-09-06 01:53:39 +0600

    feat: be a premium user page protect

--- src/pages/be-a-premium-user/index.jsx
+++ src/pages/be-a-premium-user/index.jsx
@@ -1,7 +1,17 @@
+import useProtectedRoute from "@/hooks/useProtectedRoute";
 import RootLayout from "@/layouts/RootLayout";
 import React from "react";
 
+const jwt = require("jsonwebtoken");
+
 const BeAPremiumUser = () => {
+  const accessToken =
+    typeof window !== "undefined" ? localStorage.getItem("access-token") : null;
+  const decodedToken = jwt.decode(accessToken);
+
+  // Protect Route
+  useProtectedRoute(decodedToken?.role || "guest");
+
   return (
     <div>
       <h1 className="text-3xl text-center">Coming Soon...</h1>
