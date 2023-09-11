35447d16a7c8e8982540c67bed6d473f8ac96aa0
FaizulOsman <faizulosmanrabby@gmail.com>
2023-09-12 02:52:28 +0600

    fat: users can reply on any questions

--- src/pages/discuss/index.jsx
+++ src/pages/discuss/index.jsx
@@ -5,10 +5,12 @@ import {
 } from "@/redux/discuss/discussApi";
 import { useGetMyProfileQuery } from "@/redux/user/userApi";
 import Image from "next/image";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import toast from "react-hot-toast";
+import { AiOutlineDelete } from "react-icons/ai";
 
 const Discuss = () => {
+  const [replyFormIndex, setReplyFormIndex] = useState(null);
   const accessToken =
     typeof window !== "undefined" ? localStorage.getItem("access-token") : null;
 
@@ -61,7 +63,33 @@ const Discuss = () => {
       };
       updateDiscuss({ id: d?.id, data });
     }
-    console.log(d);
+  };
+
+  const handleAddReply = ({ e, data: d }) => {
+    e.preventDefault();
+    const data = {
+      replies: [
+        ...d?.replies,
+        {
+          email: getMyProfile?.data?.email,
+          name: getMyProfile?.data?.name,
+          reply: e.target.reply.value,
+        },
+      ],
+    };
+    updateDiscuss({ id: d?.id, data });
+    e.target.reply.value = "";
+  };
+
+  const handleRemoveReply = ({ data: d, reply }) => {
+    const removeReplyAndFilterOthers = d?.replies?.filter(
+      (data) => data?.id !== reply?.id
+    );
+
+    const data = {
+      replies: removeReplyAndFilterOthers,
+    };
+    updateDiscuss({ id: d?.id, data });
   };
 
   useEffect(() => {
@@ -140,7 +168,7 @@ const Discuss = () => {
           <div key={index} className="flex gap-4 mb-4">
             <Image
               alt="Profile Image"
-              className="w-12 h-12 rounded-full border-2 p-[2px]"
+              className="w-10 h-10 rounded-full border-2 p-[2px] mt-3"
               src="https://i.ibb.co/nrtwzQd/avatar-boy.webp"
               decoding="async"
               loading="lazy"
@@ -167,17 +195,54 @@ const Discuss = () => {
                   </span>
                 </p>
                 <p className="">.</p>
-                <p className="text-blue-500 cursor-pointer">Reply</p>
+                <p
+                  className="text-blue-500 cursor-pointer"
+                  onClick={() => setReplyFormIndex(index)}
+                >
+                  Reply
+                </p>
                 <p className="">.</p>
                 <p className="text-gray-500">3y</p>
               </div>
-
+              {index === replyFormIndex && (
+                <form
+                  onSubmit={(e) => handleAddReply({ e, data })}
+                  className="flex gap-2 my-4"
+                >
+                  <input
+                    type="text"
+                    name="reply"
+                    placeholder="Reply"
+                    className="input input-bordered input-sm w-full"
+                  />
+                  <button
+                    type="submit"
+                    className="border-2 border-blue-500 rounded-full px-[6px] py-1 bg-blue-500 flex items-center justify-center cursor-pointer"
+                  >
+                    <svg
+                      stroke="currentColor"
+                      fill="none"
+                      stroke-width="2"
+                      viewBox="0 0 24 24"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      height="1em"
+                      width="1em"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="text-white"
+                    >
+                      <line x1="22" y1="2" x2="11" y2="13"></line>
+                      <polygon points="22 2 15 22 11 13 2 9 22 2"></polygon>
+                    </svg>
+                  </button>
+                </form>
+              )}
               {data?.replies?.map((reply, index) => (
                 <div key={index} className="mt-4">
-                  <div className="flex gap-4 mb-4">
+                  <div className="flex items-center gap-4 mb-4">
                     <Image
                       alt="Profile Image"
-                      className="w-12 h-12 rounded-full border-2 p-[2px]"
+                      className="w-10 h-10 rounded-full border-2 p-[2px]"
                       src="https://i.ibb.co/nrtwzQd/avatar-boy.webp"
                       decoding="async"
                       loading="lazy"
@@ -185,7 +250,15 @@ const Discuss = () => {
                       height={300}
                     />
                     <div className="">
-                      <h4 className="font-semibold">{reply?.name}</h4>
+                      <h4 className="font-semibold flex gap-4">
+                        {reply?.name}
+                        {reply?.email === getMyProfile?.data?.email && (
+                          <AiOutlineDelete
+                            onClick={() => handleRemoveReply({ data, reply })}
+                            className="text-red-500 cursor-pointer mt-1"
+                          />
+                        )}
+                      </h4>
                       <p className="text-gray-600 text-sm">{reply?.reply}</p>
                     </div>
                   </div>
