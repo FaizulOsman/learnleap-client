3743cb5f5d5c4b8face194de91dfcec5853f6bdf
FaizulOsman <faizulosmanrabby@gmail.com>
2023-09-19 13:09:31 +0600

    feat: bkash payment method added

--- src/pages/be-a-premium-user/index.jsx
+++ src/pages/be-a-premium-user/index.jsx
@@ -1,5 +1,6 @@
 import useProtectedRoute from "@/hooks/useProtectedRoute";
 import RootLayout from "@/layouts/RootLayout";
+import Image from "next/image";
 import React from "react";
 
 const jwt = require("jsonwebtoken");
@@ -13,8 +14,25 @@ const BeAPremiumUser = () => {
   useProtectedRoute(decodedToken?.role || "guest");
 
   return (
-    <div>
-      <h1 className="text-3xl text-center">Coming Soon...</h1>
+    <div className="w-11/12 max-w-[1200px] mx-auto">
+      <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-10 min-h-[50vh]">
+        <div className="border-r-2">
+          <Image
+            src="https://i.ibb.co/P1hNm8g/bkash-logo-FBB258-B90-F-seeklogo-com.png"
+            width={250}
+            height={250}
+            alt="Bkash logo"
+            className="w-28 mx-auto"
+          />
+          <h4 className="text-center font-semibold pt-4">
+            Personal No. : <span className="text-blue-500">+8801405314523</span>
+          </h4>
+          <h4 className="text-center font-semibold pt-2">
+            Reference : Your Email
+          </h4>
+        </div>
+        <div></div>
+      </div>
     </div>
   );
 };
