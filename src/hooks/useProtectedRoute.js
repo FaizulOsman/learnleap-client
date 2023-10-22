62c68d3d5396878b4da0cffc03acd8f16726cce9
FaizulOsman <faizulosmanrabby@gmail.com>
2023-10-22 20:30:15 +0600

    update: useProtectedRoute hook updated

--- src/hooks/useProtectedRoute.js
+++ src/hooks/useProtectedRoute.js
@@ -40,6 +40,25 @@ const useProtectedRoute = (role) => {
     "/dashboard/results",
     "/dashboard/saved",
   ];
+  const superAdminPages = [
+    "/bookmarks",
+    "/random-question",
+    "/be-a-premium-user",
+    "/exam/[...segments]",
+    "/exam-result/[...resultSegments]",
+    "/dashboard",
+    "/dashboard/users",
+    "/dashboard/users/update/[userId]",
+    "/dashboard/my-profile",
+    "/dashboard/test",
+    "/dashboard/test/create-test",
+    "/dashboard/test/all-test",
+    "/dashboard/exam",
+    "/dashboard/exam/create-exam",
+    "/dashboard/exam/all-exam",
+    "/dashboard/results",
+    "/dashboard/saved",
+  ];
   const guestPages = ["/"];
 
   // Define a function that redirects the user to the home page if they are not authorized
@@ -54,6 +73,9 @@ const useProtectedRoute = (role) => {
       // If not, redirect them to the home page
       redirect();
     } else if (role === "admin" && !adminPages.includes(path)) {
+      // If not, redirect them to the home page
+      redirect();
+    } else if (role === "super_admin" && !superAdminPages.includes(path)) {
       // If not, redirect them to the home page
       redirect();
     } else if (role === "guest" && !guestPages.includes(path)) {
