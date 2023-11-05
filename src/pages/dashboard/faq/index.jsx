48340dea79d7b2c33acb2960d7289f9086a58607
FaizulOsman <faizulosmanrabby@gmail.com>
2023-11-05 18:00:59 +0600

    feat: faq delete functionality incremented for admin

--- src/pages/dashboard/faq/index.jsx
+++ src/pages/dashboard/faq/index.jsx
@@ -7,18 +7,14 @@ import {
 } from "../../../redux/faq/faqApi";
 import toast from "react-hot-toast";
 import Loader from "../../../components/UI/Loader";
+import Modal from "@/components/UI/Modal/Modal";
 
 const AllFaq = () => {
   const { data: allFaq } = useGetAllFaqQuery();
   const [deleteFaq, { isSuccess, isError, error }] = useDeleteFaqMutation();
 
   const handleDeleteFaq = (faq) => {
-    const isConfirm = window.confirm(
-      "Are you sure you want to delete this Faq?"
-    );
-    if (isConfirm) {
-      deleteFaq({ id: faq?.id });
-    }
+    deleteFaq({ id: faq?.id });
   };
 
   useEffect(() => {
@@ -52,12 +48,62 @@ const AllFaq = () => {
                       <p>Question: {faq?.question}</p>
                     </div>
                     <div className="flex flex-col items-center justify-between gap-4">
-                      <button
+                      {/* <button
                         onClick={() => handleDeleteFaq(faq)}
                         className="text-2xl border-none  text-red-500 hover:text-red-600"
                       >
                         <MdDeleteOutline />
-                      </button>
+                      </button> */}
+                      <Modal
+                        Button={
+                          <MdDeleteOutline
+                            className={`text-2xl border-none  text-red-500 hover:text-red-60`}
+                          />
+                        }
+                        data={faq}
+                        modalBody={
+                          <>
+                            <h3 className="font-semibold text-md sm:text-lg text-white pb-5 text-center">
+                              Do you want to delete:{" "}
+                              <span className="text-error font-bold">
+                                {'"'}
+                                {faq?.question.slice(0, 25)}
+                                {'..."'}
+                              </span>
+                              ?
+                            </h3>
+                            <div className="py-4 text-center flex justify-around">
+                              <button
+                                onClick={() => {
+                                  handleDeleteFaq(faq);
+                                  const modal = document.getElementById(
+                                    faq?.id
+                                  );
+                                  if (modal) {
+                                    modal.close();
+                                  }
+                                }}
+                                className="btn btn-error btn-xs sm:btn-sm text-white"
+                              >
+                                Yes
+                              </button>
+                              <button
+                                onClick={() => {
+                                  const modal = document.getElementById(
+                                    faq?.id
+                                  );
+                                  if (modal) {
+                                    modal.close();
+                                  }
+                                }}
+                                className="btn btn-primary btn-xs sm:btn-sm"
+                              >
+                                No
+                              </button>
+                            </div>
+                          </>
+                        }
+                      />
                     </div>
                   </div>
                 ))}
